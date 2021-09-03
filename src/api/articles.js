ba01b481b823ce17d6909205f9673599db5bbb02
bviala <baptiste.viala@outlook.com>
2021-09-03 21:17:01 +1000

    api base url in a .env file

--- src/api/articles.js
+++ src/api/articles.js
@@ -1,6 +1,6 @@
 const getArticles = async (page, pageSize) => {
   try {
-    const response = await fetch(`https://frontend-engineering-test-api.netlify.app/.netlify/functions/articles?page=${page}&pageSize=${pageSize}`)
+    const response = await fetch(`${process.env.VUE_APP_API_BASE_URL}/articles?page=${page}&pageSize=${pageSize}`)
     return response.json()
   } catch (error) {
     throw new Error(error)
